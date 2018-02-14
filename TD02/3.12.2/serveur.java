cc90d5fd0fe4a9351114f0dc7e6e9327d766e224
Utilisateur <user@debian.arcahe.ovh>
2018-02-14 17:09:30 +0100

    TD2

--- TD02/3.12.2/serveur.java
+++ TD02/3.12.2/serveur.java
@@ -16,9 +16,22 @@ public class serveur
     }
     public void diffuser (String message)
     {
-        for (client c clients)
+        for (client c:clients)
         {
             c.serveur(message);
         }
+    }
+}
+public Main{
+    public static void main(String[]args)
+    {
+        client c1 = new client("Nathalie");
+        client c2 = new client("Lilly");
+        client c3 = new client ("Bob");
+        Serveur S = new Serveur();
+        c1.seconneter(s);
+        c2...
+        c3...
+        c3.envoyer ("bonjour");
     }
 }
